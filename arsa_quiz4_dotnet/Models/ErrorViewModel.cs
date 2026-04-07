c02017bf39976c487d4fc5f9798660b02d22a67b
Arsalaan Siddiqui <asidd101@my.centennialcollege.ca>
2026-04-07 10:33:24 -0400

    Add project files.

---
+++ arsa_quiz4_dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace arsa_quiz4_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
